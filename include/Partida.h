572051b1e68e5af63d2697f992df2a77ad663d2b
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2026-01-23 12:30:03 -0500

    hola

---
+++ include/Partida.h
@@ -0,0 +1,17 @@
+#ifndef PARTIDA_H
+#define PARTIDA_H
+
+class Partida {
+private:
+    int id;
+    int idEquipo1;
+    int idEquipo2;
+    int resultadoEquipo1;
+    int resultadoEquipo2;
+
+public:
+    void registrar();
+    void mostrar();
+};
+
+#endif // PARTIDA_H
