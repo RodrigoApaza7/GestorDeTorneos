5b43bcc019c398c55ab9f2b551ac242dc38a4413
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2026-01-23 13:41:37 -0500

    guardar partida y equipo

--- src/guardar.cpp
+++ src/guardar.cpp
@@ -9,3 +9,19 @@ void Guardar::GuardarJugador(Jugador j) {
 
     fclose(archivo); // cerrar archivo
 }
+
+void Guardar::GuardarEquipo(Equipo e) {
+    FILE* archivo;
+    archivo = fopen("equipos.bin", "a"); // abrir archivo
+
+    fwrite(&e, sizeof(Equipo), 1, archivo); // guardar objeto
+    fclose(archivo); // cerrar archivo
+}
+
+void Guardar::GuardarPartida(Partida p) {
+    FILE* archivo;
+    archivo = fopen("partidas.bin", "a"); // abrir archivo
+
+    fwrite(&p, sizeof(Partida), 1, archivo); // guardar objeto
+    fclose(archivo); // cerrar archivo
+}
