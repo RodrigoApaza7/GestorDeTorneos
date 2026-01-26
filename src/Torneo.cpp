4c5167c2ba98b46153474f63c4b1bf79c359dda8
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2026-01-25 19:40:03 -0500

    get set torneo

--- src/Torneo.cpp
+++ src/Torneo.cpp
@@ -5,6 +5,35 @@ using namespace std;
 
 Torneo::Torneo() : id(0), nombreTorneo(""), juego(""), fecha("") {}
 
+void Torneo::setfecha(const std::string& fecha) {
+    this->fecha = fecha;
+}
+
+void Torneo::setnombreTorneo(const std::string& nombreTorneo) {
+    this->nombreTorneo = nombreTorneo;
+}
+
+void Torneo::setjuego(const std::string& juego) {
+    this->juego = juego;
+}
+
+string Torneo::getid() {
+    return to_string(id);
+}
+
+string Torneo::getnombreTorneo() {
+    return nombreTorneo;
+}
+
+string Torneo::getjuego() {
+    return juego;
+}
+
+string Torneo::getfecha() {
+    return fecha;
+}
+
+
 void Torneo::registrar() {
     cout << "Registrando torneo..." << endl;
     cout << "Ingrese el nombre del torneo: ";
